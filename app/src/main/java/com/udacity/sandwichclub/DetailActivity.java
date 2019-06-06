8589b5bb3199284ce6f34d8e4d77e255ccad864e
mike <redmike@mindspring.com>
2019-06-05 21:22:53 -0400

    Json util development.  Detail activity development.  Butter knife brought into build.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,51 +4,77 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    @BindView(R.id.also_known_tv)
+    TextView mAlsoKnown;
+
+    @BindView(R.id.origin_tv)
+    TextView mOrigin;
+
+    @BindView(R.id.description_tv)
+    TextView mDescription;
+
+    @BindView(R.id.ingredients_tv)
+    TextView mIngredients;
+
+    @BindView(R.id.image_iv)
+    ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        // bind variables to UI elements
+        ButterKnife.bind(this);
 
+        // grab the intent, bail out if null
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
-        }
-
+        // the starter code miffed the else that avoids a null warning on getIntExtra
+        } else {
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
-            closeOnError();
-            return;
+
+            if (position == DEFAULT_POSITION) {
+                // EXTRA_POSITION not found in intent
+                closeOnError();
+                return;
+            }
+
+            String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+            String json = sandwiches[position];
+            Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+            if (sandwich == null) {
+                // Sandwich data unavailable
+                closeOnError();
+                return;
+            }
+
+            populateUI();
+
+            Picasso.with(this)
+                    .load(sandwich.getImage())
+                    .into(mImage);
+
+            setTitle(sandwich.getMainName());
+
         }
-
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
-            // Sandwich data unavailable
-            closeOnError();
-            return;
-        }
-
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -57,6 +83,6 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
-
+        ;
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,51 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+            // create json object, passing in string to constructor
+            JSONObject jsonObj = new JSONObject(json);
+
+            JSONObject jsonName = jsonObj.getJSONObject("name");
+            String mainName = jsonName.getString("mainName");
+
+            JSONObject aka = jsonObj.getJSONObject("mainName");
+            JSONArray akaJSONArray = aka.getJSONArray("alsoKnownAs");
+
+            // go through the list and assign the strings
+            List<String> akaStrings = new ArrayList<>(akaJSONArray.length());
+
+            for (int count = 0; count < akaJSONArray.length(); count++) {
+                akaStrings.add(akaJSONArray.getString(count));
+            }
+
+            String placeOfOrigin = jsonObj.getString("placeOfOrigin");
+            String description = jsonObj.getString("description");
+            String image = jsonObj.getString("image");
+
+            JSONArray ingredientsJsonArray = jsonObj.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>(ingredientsJsonArray.length());
+
+            for (int count1 = 0; count1 < jsonObj.length(); count1++) {
+                ingredients.add(ingredientsJsonArray.getString(count1));
+            }
+
+            return new Sandwich(mainName, akaStrings, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            // TODO: print something meaningful
+            return null;
+        }
+
     }
 }
